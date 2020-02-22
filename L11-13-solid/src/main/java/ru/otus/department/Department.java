151063e4aa1d450b69e136719653cec3c60999e7
nikolai beliaev <fr13.dev@gmail.com>
2020-02-22 20:37:52 +0300

    add atm state management for department

--- L11-13-solid/src/main/java/ru/otus/department/Department.java
+++ L11-13-solid/src/main/java/ru/otus/department/Department.java
@@ -1,6 +1,9 @@
 package ru.otus.department;
 
 import ru.otus.atm.Atm;
+import ru.otus.atm.recovering.backup.AtmBackup;
+import ru.otus.atm.recovering.backup.Backup;
+import ru.otus.atm.recovering.state.AtmState;
 import ru.otus.department.command.Command;
 
 import java.util.ArrayList;
@@ -8,7 +11,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Department {
-    private List<Atm> atms = new ArrayList<>();
+    private final List<Atm> atms = new ArrayList<>();
     private final Command totalBalance;
 
     public Department(Command totalBalance) {
@@ -19,15 +22,26 @@ public class Department {
         return totalBalance.execute(Collections.unmodifiableList(atms));
     }
 
-    public List<Atm> getAtms() {
-        return Collections.unmodifiableList(atms);
-    }
-
     public void addAtm(Atm atm) {
         atms.add(atm);
     }
 
     public int getAtmQuantity() {
         return atms.size();
+    }
+
+    public List<Backup<AtmState>> saveAtmStates() {
+        final List<Backup<AtmState>> backups = new ArrayList<>();
+        atms.forEach(i -> {
+            final Backup<AtmState> backup = new AtmBackup();
+            backup.setState(i.save());
+            backups.add(backup);
+        });
+        return backups;
+    }
+
+    public void restoreAtmStates(List<Backup<AtmState>> backups) {
+        atms.clear();
+        backups.forEach(backup -> atms.add(backup.getState().get()));
     }
 }
