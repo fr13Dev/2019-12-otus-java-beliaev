c7f2d270cd7336c596771a29b45001552cc62e61
Nick <fr13.dev@gmail.com>
2020-02-25 09:01:19 +0300

    remove field amount from class Cash

--- L11-13-solid/src/main/java/ru/otus/atm/cash/Cash.java
+++ L11-13-solid/src/main/java/ru/otus/atm/cash/Cash.java
@@ -6,20 +6,17 @@ import java.util.List;
 
 public class Cash {
     private final List<Banknote> banknotes = new ArrayList<>();
-    private int amount;
 
     public Cash(List<Banknote> banknotes) {
         this.banknotes.addAll(banknotes);
-        amount += banknotes.stream().mapToInt(Banknote::getDenomination).sum();
     }
 
     public Cash(Banknote banknote) {
         banknotes.add(banknote);
-        amount += banknote.getDenomination();
     }
 
     public int getAmount() {
-        return amount;
+        return banknotes.stream().mapToInt(Banknote::getDenomination).sum();
     }
 
     public List<Banknote> getBanknotes() {
