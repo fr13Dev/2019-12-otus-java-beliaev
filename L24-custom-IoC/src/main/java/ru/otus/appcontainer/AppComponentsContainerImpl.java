bf97102e388d827d48aab170e736a990dfe01b08
nikolai beliaev <fr13.dev@gmail.com>
2020-04-18 11:20:35 +0300

    use reflections lib

--- L24-custom-IoC/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L24-custom-IoC/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,19 +1,17 @@
 package ru.otus.appcontainer;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
 import ru.otus.appcontainer.api.AppComponentsContainer;
+import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
-import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unused")
 public class AppComponentsContainerImpl implements AppComponentsContainer {
-    private static final String CLASS_NAME_POSTFIX = ".class";
     private final Map<String, Object> container = new HashMap<>();
     private final Map<Class<?>, Reflection> reflections = new HashMap<>();
 
@@ -26,9 +24,10 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         processConfigClasses(initialConfigClasses);
     }
 
-    public AppComponentsContainerImpl(String packageName) throws IOException, ClassNotFoundException {
-        var classes = getPackageClasses(packageName);
-        processConfigClasses(classes);
+    public AppComponentsContainerImpl(String packageName) {
+        var reflections = new Reflections(packageName, new TypeAnnotationsScanner());
+        var classes = reflections.getTypesAnnotatedWith(AppComponentsContainerConfig.class, true);
+        processConfigClasses(classes.toArray(new Class[classes.size()]));
     }
 
     @Override
@@ -41,22 +40,6 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
         return (C) container.get(componentName);
-    }
-
-    private Class<?>[] getPackageClasses(String packageName) throws IOException, ClassNotFoundException {
-        var classLoader = Thread.currentThread().getContextClassLoader();
-        var path = packageName.replace(".", "/");
-        var resources = classLoader.getResources(path);
-        var files = new ArrayList<File>();
-        while (resources.hasMoreElements()) {
-            var resource = resources.nextElement();
-            files.add(new File(resource.getFile()));
-        }
-        var classes = new ArrayList<Class<?>>();
-        for (File file : files) {
-            classes.addAll(findClasses(file, packageName));
-        }
-        return classes.toArray(new Class[classes.size()]);
     }
 
     private void processConfigClasses(Class<?>[] initialConfigClasses) {
@@ -82,24 +65,5 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
             container.put(Reflection.getComponentName(bean), bean);
             container.put(reflection.getAnnotatedMethodName(method), bean);
         }
-    }
-
-    private List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        var classes = new ArrayList<Class<?>>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        var files = directory.listFiles();
-        assert files != null;
-        for (File file : files) {
-            if (file.isDirectory()) {
-                classes.addAll(findClasses(file, packageName + '.' + file.getName()));
-            } else if (file.getName().endsWith(CLASS_NAME_POSTFIX)) {
-                final String className = packageName + '.' + file.getName().replace(CLASS_NAME_POSTFIX, "");
-                final Class<?> clazz = Class.forName(className);
-                classes.add(clazz);
-            }
-        }
-        return classes;
     }
 }
