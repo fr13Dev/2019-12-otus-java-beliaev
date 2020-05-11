b77de8707e58a7c81651cfcc8c3abc9214eb0c5a
nikolai beliaev <fr13.dev@gmail.com>
2020-05-11 19:02:09 +0300

    remove unnecessary invocation

--- L28-message-system/src/main/java/ru/otus/config/AppConfig.java
+++ L28-message-system/src/main/java/ru/otus/config/AppConfig.java
@@ -43,7 +43,7 @@ public class AppConfig {
         return new SingleDatabaseMongoClientImpl(mongoClient, DB_NAME);
     }
 
-    @Bean(initMethod = "start", destroyMethod = "dispose")
+    @Bean(destroyMethod = "dispose")
     public MessageSystem messageSystem() {
         return new MessageSystemImpl();
     }
